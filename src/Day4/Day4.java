7d4768a3ebb9f7dcffb4a7382720d45bdc45d7f3
Matthew Willans <matthew.willans@viooh.com>
2021-12-04 14:51:46 +0000

    day4 part2

--- src/Day4/Day4.java
+++ src/Day4/Day4.java
@@ -1,19 +1,16 @@
 package Day4;
 
-import com.sun.jdi.connect.spi.TransportService;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Day4 {
 
-    int part1() throws IOException {
+    int part(boolean part1) throws IOException {
         List<String> lines = Files.lines(Path.of("src/Day4/input.txt")).collect(Collectors.toList());
-        List<String> bingoGame = Arrays.stream(lines.get(0).split(",")).collect(Collectors.toList());
+        LinkedList<String> bingoGame = Arrays.stream(lines.get(0).split(",")).collect(Collectors.toCollection(LinkedList::new));
         LinkedList<HashMap<Integer, String>> listOfBingoCards = new LinkedList<>();
         LinkedList<HashMap<String, Boolean>> listOfBingoCardsMarked = new LinkedList<>();
         for (int i = 2; i < lines.size() - 4; i += 6) {
@@ -29,14 +26,17 @@ public class Day4 {
             listOfBingoCards.add(bingoCardVals);
             listOfBingoCardsMarked.add(bingoCardMarked);
         }
+        Set<Integer> winners = new HashSet<>();
+        int initialNumOfCards = listOfBingoCards.size();
         for (String num : bingoGame) {
-            for (int i=0; i<listOfBingoCards.size(); i++) {
+            for (int i = 0; i < listOfBingoCards.size(); i++) {
                 HashMap<Integer, String> card = listOfBingoCards.get(i);
                 HashMap<String, Boolean> cardMarks = listOfBingoCardsMarked.get(i);
-                    if (card.containsKey(Integer.parseInt(num))) {
-                        cardMarks.put(card.get(Integer.parseInt(num)), true);
-                    }
-                    if (isValid(cardMarks)) {
+                if (card.containsKey(Integer.parseInt(num))) {
+                    cardMarks.put(card.get(Integer.parseInt(num)), true);
+                }
+                if (isValid(cardMarks)) {
+                    if (part1) {
                         int unmarkedSum = 0;
                         for (int number : card.keySet()) {
                             if (!cardMarks.get(card.get(number))) {
@@ -44,9 +44,21 @@ public class Day4 {
                             }
                         }
                         return Integer.parseInt(num) * unmarkedSum;
+                    } else {
+                        winners.add(i);
+                        if (winners.size() == initialNumOfCards) {
+                            int unmarkedSum = 0;
+                            for (int number : card.keySet()) {
+                                if (!cardMarks.get(card.get(number))) {
+                                    unmarkedSum += number;
+                                }
+                            }
+                            return Integer.parseInt(num) * unmarkedSum;
+                        }
                     }
                 }
             }
+        }
         return -1;
     }
 
@@ -63,7 +75,7 @@ public class Day4 {
 
     public static void main(String[] args) throws IOException {
         Day4 day4 = new Day4();
-        System.out.println(day4.part1());
+        System.out.println(day4.part(true));
+        System.out.println(day4.part(false));
     }
-
 }
